1304357aeb19dde94bf2bc1a854a551483a81a9a
Julien Eyriès <julien@eyries.fr>
2024-02-03 11:36:36 +0100

    customization

---
+++ _posts/2000-01-02-grandma-calculator.markdown
@@ -0,0 +1,22 @@
+---
+layout: default
+modal-id: 5
+date: 2000-01-02
+img: calculator.webp
+alt: image-alt
+project-date: 2024
+client: Myself
+category: iPad application
+description: "The perfect calculator for Grandma ! Simple and easy to use."
+---
+
+## Welcome to Grandma Calculator
+
+A free and classic calculator. No ads. No in-app purchase. No tracking.
+
+The perfect calculator for Grandma ! Simple and easy to use.
+
+[Download on the App Store](https://apps.apple.com/app/id1546631829)
+
+
+
---
+++ render.py
@@ -0,0 +1,61 @@
+
+
+from pathlib import Path
+from PIL import Image
+
+def generate_inkscape(width, svg, png):
+    import subprocess
+    subprocess.call([
+        "inkscape", 
+        "-o", str(png),
+        #"--export-width=%d" % int(width),
+        "--export-background-opacity=0",
+        "%s" % str(svg)
+        ])
+
+def generate_cairosvg(width, svg, png):
+    import cairosvg
+    print("generating", png, "from", svg)
+    cairosvg.svg2png(url=str(svg), 
+        write_to= str(png), 
+        output_width= int(width), 
+        output_height= int(width))
+
+
+
+####
+
+render_dir = Path("render")
+img_dir = Path("img")
+
+names = ["tf1", "mappy", "unibet", "dashlane", "calculator", "connect4"]
+for name in names:
+    
+    img = Image.open(render_dir / f"{name}.webp")
+    img = img.convert("RGBA")
+    img.save(render_dir / "temp" / f"{name}.png", "PNG")
+
+    #with open(resources / f"{name}.svg", "w") as f:
+    #    f.write(rendering_svg(name))
+
+    generate_inkscape(width= 512,
+                      svg= render_dir / f"{name}.svg",
+                      png= render_dir / "temp" / f"{name}-rendered.png")
+
+    img = Image.open(render_dir / "temp" / f"{name}-rendered.png")
+    img = img.convert("RGBA")
+    # set compression level
+    img.save(img_dir / "portfolio" / f"{name}.webp", "WEBP", quality= 90)
+
+
+## profile
+    
+generate_inkscape(width= 256,
+                      svg= render_dir / "profile.svg",
+                      png= render_dir / "temp" / "profile-rendered.png")
+
+img = Image.open(render_dir / "temp" / f"profile-rendered.png")
+img = img.convert("RGBA")
+# set compression level
+img.save(img_dir / "profile.webp", "WEBP", quality= 90)
+
